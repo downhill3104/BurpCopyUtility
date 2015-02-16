8d7a6ebca14b50227cd83419d333e3169fab7f47
s-yamashita <s-yamashita@yamashita-t440p>
2015-02-16 18:29:17 +0900

    1

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -1,7 +1,5 @@
 package burp;
 
-import java.awt.Color;
-import java.awt.Component;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.event.MouseAdapter;
@@ -10,15 +8,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.JMenuItem;
 import javax.swing.TransferHandler;
-import javax.swing.border.BevelBorder;
 
-public class BurpExtender implements IBurpExtender, IMessageEditorTabFactory {
+public class BurpExtender implements IBurpExtender, IContextMenuFactory {
 
 	@Override
 	/**
@@ -26,103 +25,56 @@ public class BurpExtender implements IBurpExtender, IMessageEditorTabFactory {
 	 */
 	public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks){
 		callbacks.setExtensionName("Message Copier");
-		callbacks.registerMessageEditorTabFactory(this);
+		callbacks.registerContextMenuFactory(this);
 	}
 
 	@Override
-	/**
-	 * IMessageEditorTabFactoryのメソッド
-	 */
-	public IMessageEditorTab createNewInstance(IMessageEditorController controller, boolean editable) {
-		return new CopyTab(controller, editable);
-	}
-
-	class CopyTab implements IMessageEditorTab {
-
-		private final IMessageEditorController controller;
-		private final JPanel panel = new JPanel();
-		private final JLabel label = new JLabel();
-
-		public CopyTab(IMessageEditorController controller, boolean editable) {
-			this.controller = controller;
-
-			label.setText("Copy to File by D&D !");
-			label.setBorder(new BevelBorder(BevelBorder.RAISED));
-			label.setForeground(Color.BLACK);
-			label.setBackground(Color.LIGHT_GRAY);
-			label.setOpaque(true);
-
-	        label.setTransferHandler(new TransferHandler() {
+	public List<JMenuItem> createMenuItems(final IContextMenuInvocation invocation) {
+		List<JMenuItem> miList = new ArrayList<JMenuItem>();
+		JMenuItem miCopy = new JMenuItem("Copy with Charset");
+		switch ( invocation.getInvocationContext() ) {
+		case IContextMenuInvocation.CONTEXT_PROXY_HISTORY:
+			miCopy = new JMenuItem("Copy with Charset");
+			miCopy.setTransferHandler(new TransferHandler() {
 	            @Override public int getSourceActions(JComponent c) {
 	                return COPY_OR_MOVE;
 	            }
 	            @Override protected Transferable createTransferable(JComponent c) {
-                    return new TempFileTransferable(getRequest(), getResponse());
+                    return new TempFileTransferable(invocation);
 	            }
 	        });
-	        label.addMouseListener(new MouseAdapter() {
+			miCopy.addMouseListener(new MouseAdapter() {
 	            @Override public void mousePressed(MouseEvent e) {
 //	                System.out.println(e);
 	                JComponent c = (JComponent) e.getComponent();
-	                c.getTransferHandler().exportAsDrag(c, e, TransferHandler.MOVE);
+	                c.getTransferHandler().exportAsDrag(c, e, TransferHandler.COPY);
 	            }
 	        });
-	        panel.add(label);
+			miList.add(miCopy);
+			return miList;
+		case IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_REQUEST:
+		case IContextMenuInvocation.CONTEXT_MESSAGE_EDITOR_RESPONSE:
+		case IContextMenuInvocation.CONTEXT_MESSAGE_VIEWER_REQUEST:
+		case IContextMenuInvocation.CONTEXT_MESSAGE_VIEWER_RESPONSE:
+			break;
 		}
-
-	    private byte[] getRequest() {
-	        return controller.getRequest();
-	    }
-
-	    private byte[] getResponse() {
-	        return controller.getResponse();
-	    }
-
-		@Override
-		public String getTabCaption() {
-			return "Copy";
-		}
-
-		@Override
-		public Component getUiComponent() {
-			return panel;
-		}
-
-		@Override
-		public boolean isEnabled(byte[] content, boolean isRequest) {
-			return !isRequest;
-		}
-
-		@Override
-		public void setMessage(byte[] content, boolean isRequest) {
-		}
-
-		@Override
-		public byte[] getMessage() {
-			return null;
-		}
-
-		@Override
-		public boolean isModified() {
-			return false;
-		}
-
-		@Override
-		public byte[] getSelectedData() {
-			return null;
-		}
+		return null;
 	}
 
 	class TempFileTransferable implements Transferable {
 	    private File file = null;
-	    public TempFileTransferable(byte[] request, byte[] response) {
+	    public TempFileTransferable(IContextMenuInvocation invocation) {
             try {
                 file = File.createTempFile("burp_request_response_", ".txt");
                 file.deleteOnExit();
                 FileOutputStream fos = new FileOutputStream(file);
-                fos.write(request);
-                fos.write(new String("\r\n======================================================\r\n").getBytes());
-				fos.write(response);
+    			for ( int i = 0; i < messages.length; ++i ) {
+    				buf.put(messages[i].getRequest());
+    				buf.put(new String("\r\n======================================================\r\n").getBytes());
+    				buf.put(messages[i].getResponse());
+    				buf.put(new String("\r\n======================================================\r\n").getBytes());
+    			}
+                fos.write();
 	            fos.close();
             } catch (FileNotFoundException e) {
             } catch (IOException ioe) {
@@ -137,5 +89,23 @@ public class BurpExtender implements IBurpExtender, IMessageEditorTabFactory {
 	    @Override public boolean isDataFlavorSupported(DataFlavor flavor) {
 	        return flavor.equals(DataFlavor.javaFileListFlavor);
 	    }
+
+		private byte[] getMessages(IHttpRequestResponse[] messages) {
+			long size = 0;
+			for ( int i = 0; i < messages.length; ++i ) {
+				size += messages[i].getRequest().length;
+				size += "\r\n======================================================\r\n".length();
+				size += messages[i].getResponse().length;
+				size += "\r\n======================================================\r\n".length();
+			}
+			ByteBuffer buf = ByteBuffer.allocate(size);
+			for ( int i = 0; i < messages.length; ++i ) {
+				buf.put(messages[i].getRequest());
+				buf.put(new String("\r\n======================================================\r\n").getBytes());
+				buf.put(messages[i].getResponse());
+				buf.put(new String("\r\n======================================================\r\n").getBytes());
+			}
+			return buf.array();
+		}
 	}
 }
